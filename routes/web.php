3cc296171326abddb75bed8586270aef5aa8af64
SusuMantan <suzumantan@gmail.com>
2022-10-16 02:01:52 +0700

    create about controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,34 +18,7 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/about', function () {
-    $company_bs = "Baktify started from our love for music and album, we create Baktify to spread the love in our music community, we started off from street near Rawa Belong street and now we have the most diverse collection of albums and musics. We won't stop until music spreads all over the world";
-    $company_ceo = "Conny Blue, CEO Baktify";
-    $inquirers = collect([]);
-    for ($i = 0; $i < 4; $i++) {
-        $inquirers->add([
-            "image" => "https://avatars.dicebear.com/api/micah/custom-seed.svg",
-            "name" => "Sales Inquirer",
-            "email" => "sales@baktify.com",
-            "phone" => "+62 1231 1231",
-        ]);
-    }
-    $locations = collect([]);
-    for ($i = 0; $i < 4; $i++) {
-        $locations->add([
-            "city" => "Jakarta",
-            "address" => "Jl. Rawa Belong No. 420",
-            "postal_code" => "11420"
-        ]);
-    }
-
-    return view('about', [
-        "company_bs" => $company_bs,
-        "company_ceo" => $company_ceo,
-        "inquirers" => $inquirers,
-        "locations" => $locations
-    ]);
-})->name('about');
+Route::get('/about', [AboutController::class, 'index'])->name('about');
 
 Auth::routes();
 
