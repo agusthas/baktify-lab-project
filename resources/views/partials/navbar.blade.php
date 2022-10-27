0c3b45c52b0b2b75f466d42d88a6e41e472a6f96
SusuMantan <suzumantan@gmail.com>
2022-10-27 22:35:28 +0700

    Add logo

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white fixed-top shadow-sm">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
+            <img src="{{ asset('img/logo/logo.svg') }}" alt="Logo" width="40">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
